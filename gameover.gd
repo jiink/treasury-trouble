8e6549513a7403dbe113ea0c35a3f2b742adc091
Jacob <jacob.j.carlson03@gmail.com>
2019-04-28 19:49:06 -0700

    yrjfyjfyj

--- gameover.gd
+++ gameover.gd
@@ -1,7 +1,40 @@
 extends Node2D
 
 func _ready():
-	pass
+	$ColorRect/reset_button.connect("pressed", self, "on_press")
+	$AnimationPlayer.play("drop_down")
 
 func _on_AnimationPlayer_animation_finished(anim_name):
-	get_tree().paused = true
+	if anim_name == "drop_down":
+		get_tree().paused = true
+	elif anim_name == "drop_up":
+		queue_free()
+
+func on_press():
+	print("resetting...")
+	get_tree().paused = false
+	
+	var player = $"../../sort/player"
+	player.pow_lvl = 1
+	player.spd_lvl = 1
+	player.abl_lvl = 1
+	player.update_wepstats()
+	player.alive = true
+	
+	var shop = $"../../sort/shopcart"
+	for btn in get_tree().get_nodes_in_group("upgradebuttons"):
+		btn.num_tag.text = str(1)
+	
+	var battle = $"../../battle_manager"
+	battle.wave = 1
+	battle.preptime = 15
+	battle.wavetime = 40
+	battle.state = battle.START_PREP
+
+	for guy in get_tree().get_nodes_in_group("foes"):
+		guy.die()
+	
+	for pot in get_tree().get_nodes_in_group("goldpots"):
+		pot.money = pot.money_capacity
+		
+	#$AnimationPlayer.play("drop_up")
